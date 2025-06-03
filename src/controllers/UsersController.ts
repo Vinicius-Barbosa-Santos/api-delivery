d2eb5ec7935cbf2f9c0e6758d85cb1e2d1bfcce9
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-06-03 14:28:12 -0300

    feat: registering user in the database

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,42 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { hash } from "bcrypt";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
   async create(req: Request, res: Response) {
-    res.status(201).json({ message: "User created" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, "Informe seu nome"),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "Informe um email válido" })
+        .toLowerCase(),
+      password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
+    });
+
+    const { name, email, password } = bodySchema.parse(req.body);
+
+    const userAlreadyExists = await prisma.user.findFirst({ where: { email } });
+
+    if (userAlreadyExists) {
+      throw new AppError("Usuário já cadastrado");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    res.status(201).json(userWithoutPassword);
   }
 }
 
