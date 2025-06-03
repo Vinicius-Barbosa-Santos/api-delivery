34fa92c2102c8948b95d65c40d34f0e0d97fd6a7
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-06-03 11:31:18 -0300

    feat: exception handling

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 class UsersController {
   async create(req: Request, res: Response) {
-    res.json({ message: "OK" });
+    res.status(201).json({ message: "User created" });
   }
 }
 
